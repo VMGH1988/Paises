da11b411af92f22c4d8376840742aecd205d3bbf
Victor Garcia <victor_gh_88@hotmail.com>
2024-04-11 12:27:59 +0200

    proyecto paises terminado sin refactorizar

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.services';
+import { switchMap } from 'rxjs';
+import { ByCapitalPageComponent } from '../by-capital-page/by-capital-page.component';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styles: ``
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+
+  public country?: Country;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countriesService: CountriesService,
+    private router: Router
+  ){}
+
+
+  ngOnInit(): void {
+   this.activatedRoute.params
+   // como es un observable puedo usar pipe para poder usar los map... etc
+   .pipe(
+    // switchap regresa un nuevo observable, a los parametros llamamos al metodo de nuestro servicio
+    switchMap( ({id}) =>this.countriesService.searchCountryByAlphaCode(id)),
+   )
+   .subscribe(country =>{
+    if(!country){
+      return this.router.navigateByUrl('');
+    }
+
+    return this.country = country;
+
+   });
+  }
+
+
+
 
 }
+
+
+
+
+/*
+asi era antes de usar pipe
+.subscribe((resp) =>{this.countriesService.searchCountryByAlphaCode(id)
+    .subscribe(country => {console.log({country})})}); /*hay que subscribirse para que este atentos a los cambios de la url */
+
--- src/app/countries/services/countries.services.ts
+++ src/app/countries/services/countries.services.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, delay, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -10,9 +10,127 @@ export class CountriesService {
 
   constructor(private http: HttpClient) { }
 
-  searchCapital(term: string): Observable<Country[]>{
 
-   return this.http.get<Country[]>(`${this.apiURL}/capital/${term}`)
+  private getCountriesRequest(url: string):Observable<Country[]>{
+
+    return this.http.get<Country[]>(url)
+    .pipe(
+      catchError(() => of([]))
+      );
   }
 
+  searchCountryByAlphaCode(code: string): Observable<Country | null>{
+    const url = `${this.apiURL}/alpha/${code}`;
+
+   return this.http.get<Country[]>(url)
+    //hago un map para que me devuelva el primer objeto del array ya que la api nos lo da en un array aunque sea un objeto, y si no hay nos deuelve null
+   .pipe(
+      map(countries => countries.length > 0 ? countries[0]: null),
+      catchError(() => of(null)),
+      delay(2000)
+   );
+  }
+
+
+
+  searchCapital(term: string): Observable<Country[]>{
+
+    const url = `${this.apiURL}/capital/${term}`;
+    return this.getCountriesRequest(url);
+  }
+
+
+
+  searchCountry(term: string): Observable<Country[]>{
+
+    const url = `${this.apiURL}/name/${term}`;
+    return this.getCountriesRequest(url);
+  }
+
+
+  searchRegion(region: string): Observable<Country[]>{
+
+    const url = `${this.apiURL}/region/${region}`;
+    return this.getCountriesRequest(url);
+  }
+
+
 }
+
+/*    Codigo sin refactorizar
+
+
+@Injectable({providedIn: 'root'})
+export class CountriesService {
+
+  private apiURL: string = 'https://restcountries.com/v3.1'
+
+  constructor(private http: HttpClient) { }
+
+  searchCountryByAlphaCode(code: string): Observable<Country | null>{
+    const url = `${this.apiURL}/alpha/${code}`;
+
+   return this.http.get<Country[]>(url)
+    //hago un map para que me devuelva el primer objeto del array ya que la api nos lo da en un array aunque sea un objeto, y si no hay nos deuelve null
+   .pipe(
+
+    map(countries => countries.length > 0 ? countries[0]: null),
+   catchError(() => of(null))
+   );
+  }
+
+
+
+  searchCapital(term: string): Observable<Country[]>{
+    const url = `${this.apiURL}/capital/${term}`;
+
+   return this.http.get<Country[]>(url)
+   .pipe(
+   catchError(() => of([]))
+   );
+   // con el catchError(error => of([])) captura el error y regresa un array vacio
+  }
+
+
+
+
+searchCountry(term: string): Observable<Country[]>{
+
+  const url = `${this.apiURL}/name/${term}`;
+  return this.http.get<Country[]>(url)
+}
+
+
+
+
+searchRegion(region: string): Observable<Country[]>{
+
+  const url = `${this.apiURL}/region/${region}`;
+  return this.http.get<Country[]>(url)
+}
+
+
+}
+
+
+
+
+
+
+/*
+searchCapital(term: string): Observable<Country[]>{
+    const url = `${this.apiURL}/capital/${term}`;
+
+   return this.http.get<Country[]>(url)
+   .pipe(
+    tap(countries => console.log('Paso por el tap1', countries)),
+    map( countries =>[]),
+    tap(countries => console.log('Paso por el tap2', countries)),
+   );
+   // pipe es un metodo para identificar diferentes operadores de rxjs
+   // tap Intercepta cada emisión de la fuente y ejecuta una función. Retorna un Observable idéntico a la fuente siempre y cuando no ocurra ningún error.
+   // con el map tratamos que devolvemos, si se pone map( countries =>[]) nos devuelve siempre un array vacio
+  }
+
+*/
+
